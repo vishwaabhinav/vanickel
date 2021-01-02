fbdc937e5c02e2744b9f5bcf96cc3e19ed59de6c
Abhinav Vishwa <vishwa@uber.com>
2021-01-02 17:54:59 +0530

    Update content

--- src/components/HeaderSection/header-section.component.jsx
+++ src/components/HeaderSection/header-section.component.jsx
@@ -7,13 +7,13 @@ function HeaderSection() {
       <div className="content_container d-flex flex-wrap">
         <div className="col-md-4">
             <h3 className="we-help_header">
-              We help you to achieve success in a short time
+              Innovating for simplicity
             </h3>
             <div className="short-line"></div>
-            <p className="we-help-para">
+            {/* <p className="we-help-para">
               To help our clients achieve their maximum business potential by
               harnessing the power of technology, driven by innovation
-            </p>
+            </p> */}
             <a href="#contact" className="lets-talk-btn d-flex justify-content-center align-items-center">Let&apos;s Talk</a>
         </div>
         <div className="col-md-8">
