c29706757ea4e67cc1df93439a0f324b30e34885
Abhinav Vishwa <vishwa@uber.com>
2021-01-06 16:10:57 +0530

    Update copyright year

--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="footer d-flex justify-content-between">
-            <h5 className="copy-details d-flex align-items-center m-0"><span>&copy; 2020 by Vanickel Labs &nbsp;</span> <span>(A Software Services Company)</span></h5>
+            <h5 className="copy-details d-flex align-items-center m-0"><span>&copy; {new Date().getFullYear()} by Vanickel Labs &nbsp;</span> <span>(A Software Services Company)</span></h5>
             {/* <h5 className="privacy-and-terms d-flex align-items-center m-0">
                 <p className="privacy m-0 pr-5">Privacy</p>
                 <p className="terms m-0 pl-5">Terms &amp; Conditions</p>
