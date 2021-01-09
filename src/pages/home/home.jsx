19e52942ba8ae7ee4ff4e8164e515670a8f9e992
Abhinav Vishwa <vishwa@uber.com>
2021-01-09 22:43:44 +0530

    css fix

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import AboutUs from "../../components/AboutUs/about-us.component";
 import ContactUs from "../../components/ContactUs/contact-us.component";
 import Header from "../../components/Header/header.component";
@@ -10,11 +10,20 @@ import TotalsSection from "../../components/TotalsSection/totals-section.compone
 import WeOffer from "../../components/WeOffer/we-offer.component";
 
 function Home() {
-  const { height } = useWindowDimensions();
+  const height = useWindowHeight();
+
+  const [containerHeight, setContainerHeight] = useState('auto');
+  const container = useRef(null);
+
+  useLayoutEffect(() => {
+    console.log(containerHeight, height);
+
+    setContainerHeight(Math.max(height, container.current.clientHeight));
+  });
 
   return (
     <div className="home">
-      <div className="header-section_container full-section" style={{height:height}}>
+      <div className="header-section_container full-section" ref={container} style={{height:containerHeight}}>
         <Header />
         <HeaderSection />
       </div>
@@ -26,30 +35,31 @@ function Home() {
       <ContactUs />
     </div>
   );
+
+
+  function getWindowHeight() {
+    return window.innerHeight;
+  }
+  
+  function useWindowHeight() {
+    const [wHeight, setWindowHeight] = useState(getWindowHeight());    
+  
+    useEffect(() => {
+
+      console.log(containerHeight, height);
+      function handleResize() {
+        setWindowHeight(getWindowHeight());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return wHeight;
+  }
 }
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
 
-function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
-}
 
 export default Home;
 
